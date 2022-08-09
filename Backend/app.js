e4a2aa3e6755f657ec4a2708e0b1c1aa5eb7087e
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-08-09 18:54:47 +0900

    app.js 수정중

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3306;
 
-app.get('/', (req, res) => {
+const baekjoon = require('./baekjoon.js');
+
+app.get('/userid/:id', async (req, res) => {
+
+
+    const userid = req.params.id;
+    let usertier;
+
+    const BOJ = new baekjoon();
+
+    function findTier(userid) {
+        return new Promise(function (res, rej) {
+            BOJ.baekjoonstart(userid);
+        })
+    }
+
+    await findTier(userid).then(function(tier){
+        usertier = tier;
+    });
+
+    console.log(userid);
+    console.log(usertier);
+
     res.json({
-        success: true,
+        "userid": userid,
+        "tier": usertier
     })
 })
 
